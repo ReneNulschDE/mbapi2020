bbe6bc82353de8905492c75222718de29bcea0be
ReneNulschDE.github@Nulsch.de <ReneNulschDE.github@Nulsch.de>
2024-01-14 15:07:34 +0100

    remove depricated CONNECTION_CLASS

--- custom_components/mbapi2020/config_flow.py
+++ custom_components/mbapi2020/config_flow.py
@@ -43,7 +43,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for mbapi2020."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
     def __init__(self):
         """Initialize component."""
