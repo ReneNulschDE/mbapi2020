fd1bc5f294d255b8ba40f08ee530f807e82d78bc
ReneNulschDE <ReneNulschDE.github@Nulsch.de>
2024-01-30 11:23:05 +0100

    System Health - integration disabled or deleted

--- custom_components/mbapi2020/system_health.py
+++ custom_components/mbapi2020/system_health.py
@@ -15,34 +15,40 @@ def async_register(hass: HomeAssistant, register: system_health.SystemHealthRegi
     register.async_register_info(system_health_info)
 
 
-async def system_health_info(hass):
+async def system_health_info(hass: HomeAssistant):
     """Get info for the info page."""
-
-    domain = hass.data[DOMAIN]
-    first_coordinator: MBAPI2020DataUpdateCoordinator = None
-    used_cars: int = 0
-
-    for key in iter(domain):
-        if not isinstance(domain[key], MBAPI2020DataUpdateCoordinator):
-            next
-
-        coordinator = domain[key]
-        if not first_coordinator:
-            first_coordinator = coordinator
-
-        if coordinator.client and coordinator.client.cars:
-            used_cars += len(coordinator.client.cars)
-
-    if first_coordinator and first_coordinator.client and first_coordinator.client.websocket:
-        websocket_connection_state = str(first_coordinator.client.websocket.connection_state)
-    else:
-        websocket_connection_state = "unknown"
 
     integration = await async_get_integration(hass, DOMAIN)
 
-    return {
-        "api_endpoint_reachable": system_health.async_check_can_reach_url(hass, REST_API_BASE),
-        "websocket_connection_state": websocket_connection_state,
-        "cars_connected": used_cars,
-        "version": integration.manifest.get("version"),
-    }
+    if DOMAIN in hass.data:
+        domain = hass.data[DOMAIN]
+        first_coordinator: MBAPI2020DataUpdateCoordinator = None
+        used_cars: int = 0
+
+        for key in iter(domain):
+            if not isinstance(domain[key], MBAPI2020DataUpdateCoordinator):
+                next
+
+            coordinator = domain[key]
+            if not first_coordinator:
+                first_coordinator = coordinator
+
+            if coordinator.client and coordinator.client.cars:
+                used_cars += len(coordinator.client.cars)
+
+        if first_coordinator and first_coordinator.client and first_coordinator.client.websocket:
+            websocket_connection_state = str(first_coordinator.client.websocket.connection_state)
+        else:
+            websocket_connection_state = "unknown"
+
+        return {
+            "api_endpoint_reachable": system_health.async_check_can_reach_url(hass, REST_API_BASE),
+            "websocket_connection_state": websocket_connection_state,
+            "cars_connected": used_cars,
+            "version": integration.manifest.get("version"),
+        }
+    else:
+        return {
+            "status": "Disabled/Deleted",
+            "version": integration.manifest.get("version"),
+        }
