734f170f8cee871c899b42279e13600f4419d0a0
Rene Nulsch <ReneNulschDE.github@Nulsch.de>
2025-07-07 20:34:49 +0200

    Add statistics (hack - need some rework)

--- custom_components/mbapi2020/sensor.py
+++ custom_components/mbapi2020/sensor.py
@@ -8,9 +8,9 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from homeassistant.components.sensor import RestoreSensor
+from homeassistant.components.sensor import RestoreSensor, SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_UNKNOWN
+from homeassistant.const import STATE_UNKNOWN, EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -87,6 +87,8 @@ async def async_setup_entry(
                     sensor_list.append(device)
 
     async_add_entities(sensor_list, True)
+    async_add_entities([MercedesMEWebsocketOnlineSensor(coordinator)], True)
+    async_add_entities([MercedesMEWebsocketReconnectsSensor(coordinator)], True)
 
 
 class MercedesMESensor(MercedesMeEntity, RestoreSensor):
@@ -143,3 +145,37 @@ class MercedesMESensorPoll(MercedesMeEntity, RestoreSensor):
             return STATE_UNKNOWN
 
         return self._state
+
+
+class MercedesMEWebsocketOnlineSensor(SensorEntity):
+    """Sensor für die Online-Zeit der Websocket-Connection pro Tag."""
+
+    def __init__(self, coordinator):
+        self._coordinator = coordinator
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._attr_name = "Websocket Online-Zeit heute"
+        self._attr_unique_id = f"{coordinator.config_entry.entry_id}_ws_online_time"
+        self._attr_native_unit_of_measurement = "s"
+
+    @property
+    def native_value(self):
+        # Hole Wert aus Websocket-Objekt
+        ws = self._coordinator.client.websocket
+        return getattr(ws, "_online_seconds_today", 0)
+
+
+class MercedesMEWebsocketReconnectsSensor(SensorEntity):
+    """Sensor für die Anzahl der Websocket-Initialisierungen pro Tag."""
+
+    def __init__(self, coordinator):
+        self._coordinator = coordinator
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        self._attr_name = "Websocket Verbindungs-Initialisierungen heute"
+        self._attr_unique_id = f"{coordinator.config_entry.entry_id}_ws_reconnects_today"
+        self._attr_native_unit_of_measurement = "x"
+
+    @property
+    def native_value(self):
+        # Hole Wert aus Websocket-Objekt
+        ws = self._coordinator.client.websocket
+        return getattr(ws, "_reconnects_today", 0)
